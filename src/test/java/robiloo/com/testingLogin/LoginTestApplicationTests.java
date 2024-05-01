6da204c85c3eac888c54f55a16789fcd46e499e2
RobCodesGRC <147754337+Rob-P-Smith@users.noreply.github.com>
2024-05-01 09:30:38 -0700

    Fresh Project

---
+++ src/test/java/robiloo/com/testingLogin/LoginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package robiloo.com.testingLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
